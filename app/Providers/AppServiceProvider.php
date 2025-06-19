957b29bf86d28a9d7e930eca093c50be0643f70d
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-06-19 16:58:59 +0530

    added redirection after login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,26 @@ final class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        $this->redirectAuthenticatedUser();
+    }
+
+    private function redirectAuthenticatedUser(): void
+    {
+        RedirectIfAuthenticated::redirectUsing(function () {
+            $user = Auth::user();
+
+            if ($user) {
+                $role = $user->getRoleNames()->first();
+
+                return match ($role) {
+                    'super_admin' => route('admin.dashboard'),
+                    'jobseeker' => route('jobseeker.explore'),
+                    'employer' => route('employer.dashboard'),
+                    default => '/',
+                };
+            }
+
+            return '/';
+        });
     }
 }
