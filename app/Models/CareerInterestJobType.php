b9fd3cc1b2db179b027da8bd6949b838f735b850
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-06-24 12:20:50 +0530

    added controllers

--- app/Models/CareerInterestJobType.php
+++ app/Models/CareerInterestJobType.php
@@ -1,21 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-final class CareerInterestJobType extends Model
+class CareerInterestJobType extends Model
 {
     protected $fillable = ['career_interest_id', 'job_type_id'];
 
-    public function careerInterest()
+    public function careerInterest(): BelongsTo
     {
         return $this->belongsTo(CareerInterest::class);
     }
 
-    public function jobType()
+    public function jobType(): BelongsTo
     {
         return $this->belongsTo(JobType::class);
     }
