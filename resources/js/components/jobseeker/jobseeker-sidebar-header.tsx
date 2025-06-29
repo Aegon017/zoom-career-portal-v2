e61b0a7d8bca9fbc4ff15f8cb061140da1763e22
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-06-29 21:08:26 +0530

    my jobs link changed

--- resources/js/components/jobseeker/jobseeker-sidebar-header.tsx
+++ resources/js/components/jobseeker/jobseeker-sidebar-header.tsx
@@ -5,7 +5,7 @@ import useDropdown from '@/hooks/use-dropdown';
 import { useSidebarToggle } from '@/hooks/use-sidebar-toggle';
 import { SharedData } from '@/types';
 
-export function AppSidebarHeader({ sidebarToggle }: { sidebarToggle: ReturnType<typeof useSidebarToggle> }) {
+export function AppSidebarHeader( { sidebarToggle }: { sidebarToggle: ReturnType<typeof useSidebarToggle> } ) {
     const cleanup = useMobileNavigation();
     const handleLogout = () => {
         cleanup();
@@ -21,26 +21,26 @@ export function AppSidebarHeader({ sidebarToggle }: { sidebarToggle: ReturnType<
     return (
         <div className="zc-main-top-nav">
             <button className="zc-sidebar-toggle" id="zc-sidebar-toggle"
-                ref={sidebarToggle.toggleRef}
-                onClick={() => sidebarToggle.toggle()}
+                ref={ sidebarToggle.toggleRef }
+                onClick={ () => sidebarToggle.toggle() }
             >
                 <i className="fa-solid fa-bars"></i>
             </button>
             <Link href="/jobseeker/explore" className="brand-logo">
-                <img src={logo} alt="Zoom Career" />
+                <img src={ logo } alt="Zoom Career" />
             </Link>
             <ul className="zc-top-nav-bar">
-                <li className="zc-nav-item zc-dropdown user-notification" ref={notificationNav.dropdownRef}>
-                    <button className="zc-dropdown-btn" onClick={() => {
+                <li className="zc-nav-item zc-dropdown user-notification" ref={ notificationNav.dropdownRef }>
+                    <button className="zc-dropdown-btn" onClick={ () => {
                         notificationNav.toggle();
                         userNav.close();
-                    }}>
+                    } }>
                         <div className="nicon-box">
                             <i className="fa-regular fa-bell"></i>
                             <span className="indicator">4</span>
                         </div>
                     </button>
-                    {notificationNav.isOpen && (
+                    { notificationNav.isOpen && (
                         <div className="zc-dropdown-menu show">
                             <div className="zc-dropdown-menu-header">4 New Notifications</div>
                             <div className="list-group">
@@ -49,23 +49,23 @@ export function AppSidebarHeader({ sidebarToggle }: { sidebarToggle: ReturnType<
                                 <a href="#" className="text-muted">Show all notifications</a>
                             </div>
                         </div>
-                    )}
+                    ) }
                 </li>
 
-                <li className="zc-nav-item zc-dropdown user-nav" ref={userNav.dropdownRef}>
-                    <button className="zc-dropdown-btn" onClick={() => {
+                <li className="zc-nav-item zc-dropdown user-nav" ref={ userNav.dropdownRef }>
+                    <button className="zc-dropdown-btn" onClick={ () => {
                         userNav.toggle();
                         notificationNav.close();
-                    }}>
-                        <img src={avatar} />
+                    } }>
+                        <img src={ avatar } />
                     </button>
-                    {userNav.isOpen && (
+                    { userNav.isOpen && (
                         <div className="zc-dropdown-menu show">
-                            <Link href={`/jobseeker/profile/${user.id}`} className="zc-dropdown-item">
+                            <Link href={ `/jobseeker/profile/${ user.id }` } className="zc-dropdown-item">
                                 <i className="fa-solid fa-id-badge me-2"></i>
                                 Profile
                             </Link>
-                            <Link href="/jobseeker/jobs/applied" className="zc-dropdown-item">
+                            <Link href="/jobseeker/jobs/your/applied" className="zc-dropdown-item">
                                 <i className="fa-solid fa-briefcase me-2"></i>
                                 My Jobs
                             </Link>
@@ -82,13 +82,13 @@ export function AppSidebarHeader({ sidebarToggle }: { sidebarToggle: ReturnType<
                                 method="post"
                                 href="/logout"
                                 as="button"
-                                onClick={handleLogout}
+                                onClick={ handleLogout }
                             >
                                 <i className="fa-solid fa-right-from-bracket me-2"></i>
                                 Logout
                             </Link>
                         </div>
-                    )}
+                    ) }
                 </li>
             </ul>
         </div>
