867753aef0a59eabfa90baa8c31eae79d4f5d25f
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-06-30 18:12:27 +0530

    changes

--- app/Models/CompanyUser.php
+++ app/Models/CompanyUser.php
@@ -1,30 +1,19 @@
 <?php
 
-<<<<<<< HEAD
-=======
 declare(strict_types=1);
 
->>>>>>> v3
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-<<<<<<< HEAD
-class CompanyUser extends Model
-=======
 final class CompanyUser extends Model
->>>>>>> v3
 {
     protected $fillable = [
         'company_id',
         'user_id',
         'verified_at',
-<<<<<<< HEAD
-        'status',
-=======
         'verification_status',
->>>>>>> v3
         'role',
     ];
 
