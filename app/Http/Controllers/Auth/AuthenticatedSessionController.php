7f4cee43c59a4ec745941a8bafdb3a94192f1ea9
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-07-01 19:45:37 +0530

    fixed redirection after logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -41,12 +41,16 @@ final class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+
+        if ($request->header('X-Inertia')) {
+            return response('', 409)->header('X-Inertia-Location', url('/'));
+        }
 
         return redirect('/');
     }
