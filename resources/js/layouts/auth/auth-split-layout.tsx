b1d1f74a4870f5376aec2c25575087d0f65cd4b6
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-08-25 11:29:50 +0530

    changes in the authlayout overlay colors

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -16,7 +16,7 @@ export default function AuthSplitLayout({ children, title, description, instruct
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 overflow-hidden bg-gradient-to-r from-blue-950 to-orange-800">
+                <div className="absolute inset-0 overflow-hidden bg-gradient-to-br from-blue-950 to-lime-300">
                     <BackgroundWithParticles />
                 </div>
                 <a href="/" className="relative z-20 flex items-center text-lg font-medium">
