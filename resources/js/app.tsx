e3fd0481b4d623c4f3302d491c67c7d747a91a4d
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-09-15 18:00:27 +0530

    added octane and reverb

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,28 +2,22 @@ import "../css/app.css";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { configureEcho } from "@laravel/echo-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Configure React Query
+configureEcho({
+    broadcaster: 'reverb',
+});
+
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			refetchOnWindowFocus: false, // Disable automatic refetching on window focus
-			retry: 1, // Number of retry attempts
-			staleTime: 1000 * 60 * 5, // 5 minutes cache
+			refetchOnWindowFocus: false,
+			retry: 1,
+			staleTime: 1000 * 60 * 5,
 		},
 	},
-});
-
-// Configure Echo
-configureEcho({
-	broadcaster: "pusher",
-	key: import.meta.env.VITE_PUSHER_APP_KEY,
-	cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-	forceTLS: true,
-	encrypted: true,
 });
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
